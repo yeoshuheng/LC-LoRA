abdd9f7dde1dfc7672e0c1bb7b01c7529cb5f3f6
yeoshuheng <yeoshuheng@gmail.com>
2023-12-11 19:44:55 +0800

    test: update alexnet test script

--- testing/alexnet_from_scratch.py
+++ testing/alexnet_from_scratch.py
@@ -1,17 +1,23 @@
+import glob
+import sys
 import os
-import torch
-from torchvision import datasets
+import time
 import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import scipy as spy
+from torchvision import datasets
 from torchvision import transforms
-import ssl, json
+import matplotlib.pyplot as plt
+from torch.utils.data.sampler import SubsetRandomSampler
+import ssl
+import pickle, json
 import src.main as lc
 from src.models.AlexNet import AlexNet
+import src.compression.deltaCompress as lc_compress
 from src.models.AlexNet_LowRank import getBase, AlexNet_LowRank, load_sd_decomp
-from src.utils.utils import evaluate_accuracy, lazy_restore
-import matplotlib.pyplot as plt
-
-HDFP = "/volumes/Ultra Touch" # Load HHD
-BRANCH_POINT = "model-0.522.pt" # Load branch point.
+from src.utils.utils import evaluate_accuracy, lazy_restore, evaluate_compression
 
 def data_loader():
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
@@ -32,16 +38,14 @@ def data_loader():
     return trainloader, testloader
 
 def main():
-    """
-    This script tests the 
-    """
+    HDFP = "/volumes/Ultra Touch" # Load HHD
     # Bypass using SSL unverified
     ssl._create_default_https_context = ssl._create_unverified_context
     # MNIST dataset 
     train_loader, test_loader = data_loader()
 
-    SAVE_LOC = HDFP + "/lobranch-snapshot/lobranch"
-    SAVE_LOC_FULL = HDFP + "/lobranch-snapshot/full"
+    SAVE_LOC = HDFP + "/lobranch-snapshot/lobranch/from_scratch"
+    SAVE_LOC_FULL = HDFP + "/lobranch-snapshot/full/from_scratch"
     if not os.path.exists(SAVE_LOC):
         os.makedirs(SAVE_LOC)
     if not os.path.exists(SAVE_LOC_FULL):
@@ -54,13 +58,14 @@ def main():
     model_original = AlexNet()
 
     # Load from "branch point"
-    BRANCH_LOC = HDFP + "/sim-test/alexnet/full/{}".format(BRANCH_POINT)
-    original.load_state_dict(torch.load(BRANCH_LOC))
-    model_original.load_state_dict(torch.load(BRANCH_LOC))
+    #BRANCH_LOC = HDFP + "/sim-test/alexnet/full/model-0.709.pt"
+    #original.load_state_dict(torch.load(BRANCH_LOC))
+    #model_original.load_state_dict(torch.load(BRANCH_LOC))
 
     w, b = getBase(model_original)
     model = AlexNet_LowRank(w, b, rank = 100)
-    load_sd_decomp(torch.load(BRANCH_LOC), model, DECOMPOSED_LAYERS)
+    #load_sd_decomp(torch.load(BRANCH_LOC), model, DECOMPOSED_LAYERS)
+    load_sd_decomp(model_original.state_dict(), model, DECOMPOSED_LAYERS)
     learning_rate = 0.01
     optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
     optimizer_full = torch.optim.SGD(model_original.parameters(), lr = learning_rate)
@@ -74,18 +79,30 @@ def main():
     current_iter = 0
     current_set = 0
 
-    for epch in range(1):
+    for epch in range(30):
+
+        if epch != 0:
+            # Evaluation
+            print("testing on original alexnet:")
+            full_accuracy.append(evaluate_accuracy(model_original, test_loader))
+
+            print("testing on checkpointed alexnet:")
+            restored_model = lazy_restore(base, base_decomp, bias, AlexNet(), 100, 
+                                                original.state_dict(), DECOMPOSED_LAYERS)
+            restored_accuracy.append(evaluate_accuracy(restored_model, test_loader))
+            
+
         for i, data in enumerate(train_loader, 0):
             print("Epoch: {}, Iteration: {}".format(epch, i))
             
-            if i == 100:
-                break
+            #if i == 501:
+                #break
             set_path = "/set_{}".format(current_set)
             if not os.path.exists(SAVE_LOC + set_path):
                 os.makedirs(SAVE_LOC + set_path)
                 os.makedirs(SAVE_LOC_FULL + set_path)
 
-            if i == 0: # first iteration, create baseline model
+            if i == 0 and epch == 0: # first iteration, create baseline model
                 base, base_decomp = lc.extract_weights(model, SAVE_LOC + 
                                                         "/set_{}".format(current_set), DECOMPOSED_LAYERS)
                 print("testing on first load:")
@@ -93,13 +110,14 @@ def main():
                 full_accuracy.append(init_acc)
                 decomposed_full_accuracy.append(init_acc)
                 restored_accuracy.append(init_acc)
+                
             else:
                 if i % 10 == 0: 
                     # full snapshot!
                     new_model = lazy_restore(base, base_decomp, bias, AlexNet(), 100, 
                                             original.state_dict(), DECOMPOSED_LAYERS)
                     original = new_model # Changing previous "original model" used to restore the loRA model.
-                    print("evaluating original: {}".format(evaluate_accuracy(original, test_loader)))
+                    #print("evaluating original: {}".format(evaluate_accuracy(original, test_loader)))
                     
                     current_set += 1
                     current_iter = 0
@@ -108,16 +126,17 @@ def main():
                     if not os.path.exists(SAVE_LOC + set_path):
                         os.makedirs(SAVE_LOC + set_path)
                         os.makedirs(SAVE_LOC_FULL + set_path)
-                        
-                    # Build post-snapshot full LoRA, reset model + optimiser
+                    #torch.save(original.state_dict(), SAVE_LOC + "/set_{}/branching_pt_model.pt".format(current_set))
+                    
+                    # Rebuilding LoRA layers => reset model!
                     w, b = getBase(original)
                     model = AlexNet_LowRank(w, b, rank = 100)
                     optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
                     load_sd_decomp(original.state_dict(), model, DECOMPOSED_LAYERS)
                     base, base_decomp = lc.extract_weights(model, SAVE_LOC + 
                                                         "/set_{}".format(current_set), DECOMPOSED_LAYERS)
-                    print("testing on full snapshot:")
-                    evaluate_accuracy(model, test_loader)
+                    #print("testing on full snapshot:")
+                    #evaluate_accuracy(model, test_loader)
 
                 else:
                     # Delta-compression
@@ -133,22 +152,9 @@ def main():
                     base_decomp = new_base_decomp
 
                     current_iter += 1
-            
-            if current_iter != 0 and current_iter % 2 == 0:
-                print("testing on decomposed alexnet:")
-                decomposed_full_accuracy.append(evaluate_accuracy(model, test_loader))
-                print("testing on checkpointed alexnet:")
-                restored_model = lazy_restore(base, base_decomp, bias, AlexNet(), 100, 
-                                            original.state_dict(), DECOMPOSED_LAYERS)
-                restored_accuracy.append(evaluate_accuracy(restored_model, test_loader))
 
             torch.save(model_original.state_dict(), SAVE_LOC_FULL + "/set_{}/base_model_{}.pt".format(current_set, 
                                                                                                     current_iter))
-            
-            if current_iter != 0 and current_iter % 2 == 0:
-                # Evaluation
-                print("testing on original alexnet:")
-                full_accuracy.append(evaluate_accuracy(model_original, test_loader))
             
             # ==========================
             # Training on Low-Rank Model
@@ -178,25 +184,9 @@ def main():
             loss_full = torch.nn.functional.cross_entropy(outputs_full,labels)
             loss_full.backward()
             optimizer_full.step()
-        
-    write_path = HDFP + "/lobranch-snapshot/full_acc_results.json"
+
+    write_path = HDFP + "/lobranch-snapshot/from-scratch-full_acc_results_1000.json"
     with open(write_path, 'w') as f:
         json.dump({"full_model" : full_accuracy, 
                     "decomposed_model" : decomposed_full_accuracy, 
                     "decomposed_restored" : restored_accuracy}, f)
-    plot_path = "/lobranch-snapshot"
-
-    plt.figure(figsize = (20, 5))
-    plt.title("AlexNet Accuracy, Decomposed Models against Full Model")
-    plt.plot(full_accuracy, label = "Full Model") 
-    plt.plot(decomposed_full_accuracy, label = "Decomposed Model")
-    plt.plot(restored_accuracy, label = "Restored Decomposed Model")
-    plt.legend()
-    plt.savefig(plot_path + "acc_comparison_results.png")
-
-    plt.figure(figsize = (20, 5))
-    plt.title("AlexNet Absolute Accuracy Loss")
-    plt.plot(np.abs(np.subtract(np.array(decomposed_full_accuracy), 
-                        np.array(restored_accuracy))))
-    plt.legend()
-    plt.savefig(plot_path + "acc_loss_results.png")
